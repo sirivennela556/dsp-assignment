e927f88a603729b915e980fd4f8fdac2c403b09b
sirivennela556 <78716827+sirivennela556@users.noreply.github.com>
2021-02-21 19:26:27 +0530

    Add files via upload

---
+++ moving_average_system.py
@@ -0,0 +1,18 @@
+import numpy as np;
+from matplotlib import pyplot as plt
+x[n]=[1,2,1,2,3,4];
+n=(0,5);
+y[n]=0;
+M=3;
+for(n=0::10);
+{
+	for(int k=0::m-1);
+	{
+		if((n-k)>=0);
+		{
+			y[n]=y[n]+x[n-k];
+		}
+	plt.plot(n,y[n]);plt.show();
+	}
+}
+
