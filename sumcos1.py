424feee5fa1a8526cd6f7b0cc84bdbf79661ad94
sirivennela556 <78716827+sirivennela556@users.noreply.github.com>
2021-02-21 16:06:03 +0530

    Add files via upload

---
+++ sumcos1.py
@@ -0,0 +1,14 @@
+import numpy as np
+from matplotlib import pyplot as plt
+n=np.arange(1,250)
+F1=250;p1=np.pi/3
+F2=250;p2=np.pi
+Fs=10000;
+s1=0.5*np.cos(2*np.pi*F1/Fs*n+np.pi/2+p1)
+plt.subplot(311);
+plt.stem(n,s1);
+s2=0.5*np.cos(2*np.pi*F2/Fs*n+np.pi/2+p2)
+plt.subplot(312);
+plt.stem(n,s2);
+plt.subplot(313);
+plt.stem(n,s1+s2);plt.show();
